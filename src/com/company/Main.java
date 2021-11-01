a18236e7919c85757632b876ffbbacb602f8cd73
jakyp003 <daiyrbekovjakypbek003@gmail.com>
2021-11-01 15:46:35 +0300

    first push

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ageOfman = 20-45;
+        int temperature = 20-30;
+        boolean isItNight = true;
+
+        if (ageOfman > 30 && temperature > 15);{
+            System.out.println("you can go for a walk");
+        }
+
+
+    }
+}
