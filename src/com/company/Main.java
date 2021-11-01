042a3a99154e18e4d6f05f115066c31ced48b376
jakyp003 <daiyrbekovjakypbek003@gmail.com>
2021-11-01 19:20:38 +0300

    adding a README File to the repository

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,38 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        // write your code here
+        System.out.println(tempAge(30,35));
+        System.out.println(tempAge(50,20));
+        System.out.println(tempAge(7,-10));
+        System.out.println(tempAge(15,35));
+        System.out.println(tempAge(60,35));
 
-        int ageOfman = 20-45;
-        int temperature = 20-30;
-        boolean isItNight = true;
 
-        if (ageOfman > 30 && temperature > 15);{
-            System.out.println("you can go for a walk");
-        }
 
 
     }
+
+    public static String tempAge(int age, int temp) {
+
+
+        String res1 = "можно идти гулять";
+        String res2 = "оставайтесь дома";
+
+        if (age >= 20 && age <= 45 && temp >= -20 && temp <= 30) {
+            return res1;
+        } else if (age <= 20 && temp > 0 && temp <= 28) {
+            return res1;
+        } else if (age >= 45 && temp >= -10 && temp <= 25) {
+            return res1;
+        } else {
+            return res2;}
+    }
+
+
+
 }
